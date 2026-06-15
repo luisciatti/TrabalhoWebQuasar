e4ac701a7d18981b5b8cc9d607bd616e31da0920
Luis Eduardo Abreu <luisciatteabreu@gmail.com>
2026-06-15 18:07:18 -0300

    Adicionado novas funções para as tabelas

--- email-template-hub/src/layouts/MainLayout.vue
+++ email-template-hub/src/layouts/MainLayout.vue
@@ -3,10 +3,8 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-          Quasar App
+          Email Template Hub
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
